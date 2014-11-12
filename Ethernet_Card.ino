9ebb02628dc823eafa9d13c21b062154528515d0
miky90 <marco.michielotto@gmail.com>
2014-11-12 01:50:38 +0100

    aggiunta a git

---
+++ Ethernet_Card.ino
@@ -0,0 +1,146 @@
+/*
+*  File:     Ethernet_Card.ino
+*  Author:   Marco Michielotto
+*  Date:     01/06/2014
+*  Hardware: Arduino Pro Mini(5V, 16Mhz)w/ ATmega328,
+*            ENC28J60 on SPI connection (see below)
+*            NRF2401L+ on SPI connection (see below)
+*/
+
+//Connect the nRF24L01 to Arduino like this.
+// \code
+//                 Arduino      NRF2401L+
+//                 3V3----------VCC   (3.3v) 
+//             pin D8-----------CE    (chip enable in)
+//          SS pin D9-----------CSN   (chip select in)
+//         SCK pin D13----------SCK   (SPI clock in)
+//        MOSI pin D11----------SDI   (SPI Data in)
+//        MISO pin D12----------SDO   (SPI data out)
+//                              IRQ   (Interrupt output, not connected)
+//                 GND----------GND   (ground in)
+// \endcode
+//Connect the ENC28J60 to Arduino like this.
+//
+//                 Arduino      ENC28J60
+//                 3V3----------VCC
+//             pin RESET--------RESET
+//          SS pin D10----------CS    (chip select in)
+//         SCK pin D13----------SCK   (SPI clock in)
+//        MOSI pin D11----------SI    (SPI Data in)
+//        MISO pin D12----------SO    (SPI data out)
+//                              INT   ()
+//                              WDL   (not connected)
+//                              CLKOUT(not connected)
+//                 GND----------GND   (ground in)
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <EtherCard.h> 
+#include <Time.h>
+#include <Timezone.h>
+
+#define MY_ADDRESS 00
+// nRF24L01(+) radio attached using Getting Started board 
+RF24 radio(8,9);
+
+// Network uses that radio
+RF24Network network(radio);
+
+// Structure of our payload
+struct payload_Meteo
+{
+  time_t time;
+  float inTemp;
+  float inHum;
+  float outTemp;
+  float outHum;
+  float outPress;
+};
+
+//Ethernet parameters
+static byte mac[] = {0x00,0x50,0x56,0xAE,0xB3,0xA6};
+static byte ip[] = {192,168,1,150};
+byte Ethernet::buffer[700];
+#define ENC28J60_CS 10
+char website[] PROGMEM= "www.web-plus.it"; // www.TUO_SITO_WEB.it
+char buffer[70];
+unsigned long time_last = 0; 
+uint8_t pending_data = 0;
+byte interval = 30; // seconds - intervello di tempo tra un invio di dati ed il successivo verso il server
+
+void setup()
+{ 
+  Serial.begin(9600);
+  Serial.println("REBOOT");
+  
+  SPI.begin();
+  radio.begin();
+  network.begin(/*channel*/ 10, /*node address*/ MY_ADDRESS);
+  
+  //inizializza e testa Ethernet
+  Serial.print("Verifica Enc28j60 ... ");
+  if ( ether.begin( sizeof Ethernet::buffer, mac, ENC28J60_CS) )
+    Serial.println("\tsuccess");
+  else {
+    Serial.println("\tfailed");
+  }
+  Serial.print("Setting IP ... ");
+  if ( ether.dhcpSetup() ) {
+    Serial.println("\t\tsuccess");
+  } 
+  else {
+    if ( ether.staticSetup(ip) ) 
+      Serial.println("\t\tsuccess");
+    else 
+      Serial.println("\t\tfailed");
+  }
+  Serial.print("Verifica sito web ... ");
+  if ( ether.dnsLookup( website ) ) 
+    Serial.println("\tsuccess");
+  else 
+    Serial.println("\tfailed");
+  Serial.println();
+  Serial.print("Setup eseguito in :");
+  Serial.print(millis()/1000);
+  Serial.println("s");
+  Serial.println();
+  Serial.println();
+}
+
+payload_Meteo payload;
+
+void loop()
+{
+  // Pump the network regularly
+  network.update();
+   // Is there anything ready for us?
+  while ( network.available() )
+  {
+    // If so, grab it and print it out
+    RF24NetworkHeader header;
+    network.read(header,&payload,sizeof(payload));
+    Serial.print("Received packet = time: ");
+    Serial.print(payload.time);
+    Serial.print(" inTemp: ");
+    Serial.print(payload.inTemp);
+    Serial.print(" inHum: ");
+    Serial.print(payload.inHum);
+    Serial.print(" outTemp: ");
+    Serial.print(payload.outTemp);
+    Serial.print(" outHum: ");
+    Serial.print(payload.outHum);
+    Serial.print(" Press: ");
+    Serial.println(payload.outPress);
+    pending_data = 1;
+  }
+  ether.packetLoop(ether.packetReceive());
+  if(pending_data) {
+    if (millis()/1000 > time_last){
+      if ( (time_last = millis()/1000) % interval == 0 ){
+        invioDati();
+      }
+    }
+  }
+
+}
---
+++ funzioni.ino
@@ -0,0 +1,125 @@
+static void response_callback (byte status, word off, word len) {
+ 
+  Serial.println();
+  Serial.println( (char *) Ethernet::buffer + off);
+  Serial.println();
+  int indice=0;
+  uint8_t flag = 0;
+  while(indice < len){
+    if(buffer[indice++]!='"')
+      flag=1;
+  }
+  if(flag==1){
+    pending_data=0;
+    String risposta;
+    risposta += buffer[indice];
+    risposta += buffer[indice+1];
+    if(risposta.equals("ok")) {
+      Serial.println("parametri inviati");
+    }
+    else if(buffer[indice]=='e'){
+      Serial.println("errore invio");
+    }
+  }
+  else {
+     Serial.println("sito inaccessibile");
+  }
+}
+
+
+static void invioDati() {
+  
+    //IN
+    //consiero temperature da 80 a -40 (99 a -99 ^^)
+    char* s_in_temp;
+    if(payload.inTemp < 10 && payload.inTemp >= 0) {
+      //x.xx 
+      s_in_temp = new char[5];
+      dtostrf(payload.inTemp, 1, 2, s_in_temp);
+    }
+    else if(payload.inTemp >=10 | payload.inTemp < 0) { 
+      //xx.xx e -x.xx 
+      s_in_temp = new char[6];
+      dtostrf(payload.inTemp, 2, 2, s_in_temp);
+    }
+    else {
+      //-xx.xx
+      s_in_temp = new char[7];
+      dtostrf(payload.inTemp, 3, 2, s_in_temp);
+    }
+    //consiero umidita da 0 a 100
+    char* s_in_hum;
+    if(payload.inHum < 10 ) {
+      //x.xx 
+      s_in_hum = new char[5];
+      dtostrf(payload.inHum, 1, 2, s_in_hum);
+    }
+    else if(payload.inHum >=10 ) { 
+      //xx.xx 
+      s_in_hum = new char[6];
+      dtostrf(payload.inHum, 2, 2, s_in_hum);
+    }
+    else {
+      //100.00
+      s_in_hum = new char[7];
+      dtostrf(payload.inHum, 3, 2, s_in_hum);
+    }
+    //OUT
+    //consiero temperature da 80 a -40 (99 a -99 ^^)
+    char* s_out_temp;
+    
+    if(payload.outTemp < 10 && payload.outTemp >= 0) {
+      //x.xx 
+      s_out_temp = new char[5];
+      dtostrf(payload.outTemp, 1, 2, s_out_temp);
+    }
+    else if(payload.outTemp >=10 | payload.outTemp < 0) { 
+      //xx.xx e -x.xx 
+      s_out_temp = new char[6];
+      dtostrf(payload.outTemp, 2, 2, s_out_temp);
+    }
+    else {
+      //-xx.xx
+      s_out_temp = new char[7];
+      dtostrf(payload.outTemp, 3, 2, s_out_temp);
+    }
+    //consiero umidita da 0 a 100
+    char* s_out_hum;
+    if(payload.outHum < 10 ) {
+      //x.xx 
+      s_out_hum = new char[5];
+      dtostrf(payload.outHum, 1, 2, s_out_hum);
+    }
+    else if(payload.outHum >=10 ) { 
+      //xx.xx 
+      s_out_hum = new char[6];
+      dtostrf(payload.outHum, 2, 2, s_out_hum);
+    }
+    else {
+      //100.00
+      s_out_hum = new char[7];
+      dtostrf(payload.outHum, 3, 2, s_out_hum);
+    }
+    //PRESSIONE
+    //considero pressioni da 100 a 9999 hahaha
+     //consiero umidita da 0 a 100
+    char* s_out_press;
+    if(payload.outPress < 1000 ) {
+      //xxx.xx 
+      s_out_press = new char[7];
+      dtostrf(payload.outPress, 3, 2, s_out_press);
+    }
+    else {
+      //xxxx.xx
+      s_out_press = new char[8];
+      dtostrf(payload.outPress, 4, 2, s_out_press);
+    }
+     
+    sprintf(buffer, "inTemp=%s&inHum=%s&outTemp=%s&outHum=%s&press=%s", s_in_temp, s_in_hum, s_out_temp, s_out_hum, s_out_press);
+    ether.browseUrl(PSTR("/arduino.php?"), buffer, website, response_callback);
+    delete s_in_temp;
+    delete s_in_hum;
+    delete s_out_temp;
+    delete s_out_hum;
+    delete s_out_press;
+}
